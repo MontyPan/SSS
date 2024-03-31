c9221796abb8dc43c4a3ba8a317b3e1fb6069ffe
MontyPan <monty.pan@dontcareabout.us>
2024-03-31 09:47:51 +0800

    增加：MAX_GRADE / MAX_SERIAL

--- src/main/java/us/dontcareabout/sss/client/Util.java
+++ src/main/java/us/dontcareabout/sss/client/Util.java
@@ -1,6 +1,10 @@
 package us.dontcareabout.sss.client;
 
 public class Util {
+	//XXX 遙遠的未來應該抽出去變成 host page 提供？
+	public static final int MAX_GRADE = 3;
+	public static final int MAX_SERIAL = 4;
+
 	/**
 	 * @param grade 年級
 	 * @param serial 班級序號
