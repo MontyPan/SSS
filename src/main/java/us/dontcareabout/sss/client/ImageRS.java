ca8a8ce359c20857892243b6dc403532ed384b55
MontyPan <monty.pan@dontcareabout.us>
2024-04-15 00:03:31 +0800

    增加：ImageRS 以及 3 個圖檔

---
+++ src/main/java/us/dontcareabout/sss/client/ImageRS.java
@@ -0,0 +1,13 @@
+package us.dontcareabout.sss.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface ImageRS extends ClientBundle {
+	static ImageRS I = GWT.create(ImageRS.class);
+
+	ImageResource calendarCheck();
+	ImageResource pencilSquare();
+	ImageResource personCheck();
+}
