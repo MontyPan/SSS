a154514c565e1d5482231204a785a32246eff085
MontyPan <monty.pan@dontcareabout.us>
2024-04-18 11:15:45 +0800

    修正：重複增加 root 的錯誤

--- src/main/java/us/dontcareabout/sss/client/ui/SemesterSchedule.java
+++ src/main/java/us/dontcareabout/sss/client/ui/SemesterSchedule.java
@@ -44,7 +44,7 @@ public class SemesterSchedule extends LayerContainer {
 		root.addChild(staticCol, 72);
 		weekClmn.stream().forEach(wc -> root.addChild(wc, 100));
 
-		addLayer(root);
+		root.redeploy();
 
 		//XXX 由 root 決定自身大小會出現的程式碼，有沒有辦法不用每次搞這些呢？
 		root.resize(1, Integer.MAX_VALUE);	//XXX 數值給太小，裡頭的 child 可能不會觸發 adjustMember()
