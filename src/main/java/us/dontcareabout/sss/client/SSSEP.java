3fc8bb06919978245015ead32f699796d3b5138a
MontyPan <monty.pan@dontcareabout.us>
2024-07-06 11:35:05 +0800

    POC 版本的 EP

--- src/main/java/us/dontcareabout/sss/client/SSSEP.java
+++ src/main/java/us/dontcareabout/sss/client/SSSEP.java
@@ -2,13 +2,19 @@ package us.dontcareabout.sss.client;
 
 import com.google.gwt.user.client.Window;
 
-import us.dontcareabout.gwt.client.GFEP;
+import us.dontcareabout.gst.client.GSTEP;
+import us.dontcareabout.gst.client.data.SheetIdDao;
+import us.dontcareabout.gxt.client.component.RwdRootPanel;
+import us.dontcareabout.sss.client.data.DataCenter;
+import us.dontcareabout.sss.client.ui.MainView;
 
-public class SSSEP extends GFEP {
-	public SSSEP() {}
+public class SSSEP extends GSTEP {
+	public SSSEP() {
+		super("SSS-Key", "1zaEouhkIQYorcPkVBtdTZod2mKqhH_Au5-8d42CerIo");
+	}
 
 	@Override
-	protected String version() { return "0.0.1"; }
+	protected String version() { return "POC"; }
 
 	@Override
 	protected String defaultLocale() { return "zh_TW"; }
@@ -20,5 +26,7 @@ public class SSSEP extends GFEP {
 
 	@Override
 	protected void start() {
+		RwdRootPanel.setComponent(new MainView());
+		DataCenter.wantSchedule(SheetIdDao.priorityValue(), 112, false); //Refactory magic number
 	}
 }
